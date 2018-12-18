9daba57a9d8d9fafdc21a586f4b11d9e990385dc
Kaylee Bunce <kaybunce@hotmail.com>
2018-12-18 16:01:06 -0700

    Add functionality to update state with input change and handle the submit of the form

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -10,14 +10,30 @@ class SignUp extends Component {
     }
   }
 
+  handleChange = (e) => {
+    const { name, value } = e.target
+    this.setState({
+      [name]: value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.setState({
+      name: '',
+      email: '',
+      password: ''
+    })
+  }
+
   render() {
     const {name, email, password} = this.state
     return (
-      <form>
-        <input name="name" value={name}/>
-        <input name="email" value={email}/>
-        <input name="password" value={password}/>
-        <button>SUBMIT</button>
+      <form onSubmit={ this.handleSubmit }>
+        <input name="name" value={name} onChange={ this.handleChange } />
+        <input name="email" value={email} onChange={ this.handleChange } />
+        <input name="password" value={password} onChange={ this.handleChange } />
+        <button className='submit-sign-up'>SUBMIT</button>
       </form> 
     )
   }
