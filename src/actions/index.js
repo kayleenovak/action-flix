c98dad003e60181e51bea812362e9a465f7c8ead
Derek Hanciles <drhanciles@icloud.com>
2018-12-19 11:21:56 -0700

    Implemented actions for redux thunk

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+export const isLoading = (check) => ({
+  type: 'IS_LOADING', 
+  isLoading: check
+})
+
+export const hasErrored = (check) => ({
+  type: 'HAS_ERRORED', 
+  hasErrored: check
+})
+
+export const fetchDataSuccess = (movies) => ({
+  type: 'FETCH_DATA_SUCCESS', 
+  movies
+})
