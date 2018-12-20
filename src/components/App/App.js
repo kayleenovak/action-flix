613eb05e848d3958b0cda431ca6bb3bef7bad3ea
Derek Hanciles <drhanciles@icloud.com>
2018-12-19 18:05:47 -0700

    A test for cleanMovies function has been implemented and is passing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import * as API from '../../apiCalls/apiCalls.js'
 import './App.css';
 import { Route } from 'react-router-dom'
 import  MovieContainer  from '../MovieContainer/MovieContainer'
