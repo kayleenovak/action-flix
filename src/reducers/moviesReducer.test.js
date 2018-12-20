f94ceaae8046da278378310f6bf2963c8a2dc3f7
Derek Hanciles <drhanciles@icloud.com>
2018-12-20 09:57:08 -0700

    Test checking if moviesReducer is updating and state has been implemented and is passing

--- src/reducers/moviesReducer.test.js
+++ src/reducers/moviesReducer.test.js
@@ -1,5 +1,7 @@
 import { moviesReducer } from './moviesReducer'
 import * as actions from '../actions'
+import { movies } from '../../src/mockData.js'
+
 
 describe('moviesReducer', () => {
   it('should return default state', () => {
@@ -9,4 +11,14 @@ describe('moviesReducer', () => {
 
     expect(result).toEqual(expected)
   })
+  it('should update state with movies requested if our fetch request is successful', () => {
+    const  mockMovies = movies.results
+    const expected = mockMovies
+
+    const result = moviesReducer([], actions.fetchDataSuccess(mockMovies))
+
+    expect(result).toEqual(expected)
+
+  })
+
 })
