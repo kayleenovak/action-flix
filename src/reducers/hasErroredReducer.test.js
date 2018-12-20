adf3ae6aa081aaec840ac67798b6905fcf94128a
Derek Hanciles <drhanciles@icloud.com>
2018-12-19 22:09:50 -0700

    Test checking if state will be updated in hasErroredReducer.test file has been impelemented and is passing

--- src/reducers/hasErroredReducer.test.js
+++ src/reducers/hasErroredReducer.test.js
@@ -9,4 +9,15 @@ describe('hassErroredReducer', () => {
 
     expect(result).toEqual(expected)
   })
+  it('should update state if there is an error', () => {
+    const action = {
+      type: 'HAS_ERRORED', 
+      hasErrored: true
+    }
+    const expected = true
+
+    const result = hasErroredReducer(false, action)
+
+    expect(result).toEqual(expected)
+  })
 })
