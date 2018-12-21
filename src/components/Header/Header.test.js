968638f260469b61d5adac55cf89092907290d58
Derek Hanciles <drhanciles@icloud.com>
2018-12-20 18:52:39 -0700

    Test checking if dispatch is called when action is dispatched has been impelemented

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -1,11 +1,30 @@
 import React from 'react'
 import { shallow, mount } from 'enzyme'
 import { Header } from './Header'
+import { mapDispatchToProps } from '../MovieContainer/MovieContainer';
 
 describe('Header', () => {
+  let wrapper
+  let mockLogOut
+  beforeEach(() => {
+    mockLogOut = jest.fn()
+    wrapper = shallow(<Header logOutUser={ mockLogOut }/>)
+  })
   it('should match the snapshot', () => {
-    const wrapper = shallow(<Header />); 
     
     expect(wrapper).toMatchSnapshot();
   });
+  describe('mapDispatchToProps', () => {
+    it('should call dispatch when action is dipatched', () => {
+      const mockDispatch = jest.fn()
+      const isLoggedOut = mapDispatchToProps(mockDispatch)
+      
+      expect(isLoggedOut).toHaveBeencalled
+
+    })
+  })
+
+  it.skip('should handle a click event that will log out our user', () => {
+
+  })
 })
