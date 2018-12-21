d4c95ce024c9d2d8ddd7b1da246c7855332807e1
Derek Hanciles <drhanciles@icloud.com>
2018-12-21 09:36:13 -0700

    Test checking if Header is handling event has been implemented and is passing

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -8,23 +8,25 @@ describe('Header', () => {
   let mockLogOut
   beforeEach(() => {
     mockLogOut = jest.fn()
-    wrapper = shallow(<Header logOutUser={ mockLogOut }/>)
+    wrapper = shallow(<Header logUserOut={ mockLogOut }/>)
   })
   it('should match the snapshot', () => {
     
     expect(wrapper).toMatchSnapshot();
   });
+  it('should handle a click event', () => {
+    wrapper.find('.logout-btn').simulate('click')
+    window.alert('clicked')
+
+    expect(mockLogOut).toHaveBeenCalled
+  })
   describe('mapDispatchToProps', () => {
     it('should call dispatch when action is dipatched', () => {
       const mockDispatch = jest.fn()
       const isLoggedOut = mapDispatchToProps(mockDispatch)
       
-      expect(isLoggedOut).toHaveBeencalled
+      expect(mockDispatch).toHaveBeenCalled
 
     })
-  })
-
-  it.skip('should handle a click event that will log out our user', () => {
-
-  })
+  });
 })
