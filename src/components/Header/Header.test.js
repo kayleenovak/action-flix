91f5527bc058872f2bb79ca2bebcd4db77812673
Derek Hanciles <drhanciles@icloud.com>
2018-12-26 16:19:31 -0700

    Test checking if mapDispatchToProps is being called on the click of the button has been impelemented and is passing

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { shallow, mount } from 'enzyme'
 import { Header } from './Header'
 import { mapDispatchToProps } from '../MovieContainer/MovieContainer';
+import { logOut } from '../../actions/index.js'
 
 describe('Header', () => {
   let wrapper
@@ -16,15 +17,15 @@ describe('Header', () => {
   });
   it('should handle a click event', () => {
     wrapper.find('.logout-btn').simulate('click')
-    window.alert('clicked')
 
     expect(mockLogOut).toHaveBeenCalled
   })
   describe('mapDispatchToProps', () => {
-    it('should call dispatch when action is dipatched', () => {
-      const mockDispatch = jest.fn()
-      const isLoggedOut = mapDispatchToProps(mockDispatch)
-      
+    it('calls dispatch with a logOut action when the logout button is clicked', () => {
+      const mockDispatch = jest.fn() 
+
+      const mappedProps = mapDispatchToProps(mockDispatch)
+
       expect(mockDispatch).toHaveBeenCalled
 
     })
