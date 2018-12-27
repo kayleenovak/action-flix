25b300703911e7b830c86aa26c98a40fbc1758a9
Joel Smith <joelwsmith123@gmail.com>
2018-12-27 12:29:08 -0700

    Connect movieCard to store

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,19 +1,31 @@
 import React from 'react'
 import './MovieCard.css'
+import { connect } from 'react-redux'
 
-export const MovieCard = ({poster, title, rating, releaseDate, description}) => {
+
+export const MovieCard = ({title, posterPath, releaseDate, voteAverage, overview, props}) => {
   return (
     <article className='movie-card'>
-      <img className='movie-img' src={poster} alt='movie poster' />
+      <img className='movie-img' src={posterPath} alt='movie poster' />
       <div className='movie-info-text'>
         <div className='movie-icon-container'>
-          <p className='movie-rating'>{rating}</p>
-          <img src='../images/full-popcorn.svg' alt='full popcorn' />
+          <p className='movie-rating'>{voteAverage}</p>
+          <img src='../images/full-popcorn.svg' alt='full popcorn' onClick={() => props.addFavorite(movieId, props.userId, title, posterPath, releaseDate, voteAverage, overview)} />
         </div>
         <h3 className='movie-title'>{title}</h3>
         <p className='movie-release'>{releaseDate}</p>
-        <p className='movie-description'>{description}</p>
+        <p className='movie-description'>{overview}</p>
       </div>
     </article>
   )
+
+  export const mapDispatchToProps = (dispatch) => ({
+    addFavorite: () => dispatch(postFavorite(movieId, userId, title, posterPath, releaseDate, voteAverage, overview))
+  })
+
+  export const mapStateToProps = (state) => ({
+    userId: state.userId
+  })
+
+  export default connect(mapStateToProps, mapDispatchToProps)(MovieCard)
 }
