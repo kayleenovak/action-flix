6ed2f36739fea92190988d92a3f805c6bd19af2d
Derek Hanciles <drhanciles@icloud.com>
2018-12-28 15:26:49 -0700

    Buttons in login/signup form are now properly disabled and renabling

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -3,6 +3,7 @@ import { Redirect } from 'react-router-dom'
 import { getUser } from '../../thunks/signIn.js'
 import { createUser } from '../../thunks/createUser.js'
 import { connect } from 'react-redux'
+import './SignUp.css'
 
 export class SignUp extends Component {
   constructor() {
@@ -11,7 +12,8 @@ export class SignUp extends Component {
       name: '', 
       email: '', 
       password: '',
-      showSignIn: true
+      showSignIn: true, 
+      disableButton: true
     }
   }
 
@@ -19,8 +21,18 @@ export class SignUp extends Component {
     const { name, value } = e.target
     this.setState({
       [name]: value
-    })
-  }
+    }, () => { 
+      if(this.state.showSignIn && this.state.email !== '' && this.state.password  !== '') {
+        this.setState({
+          disableButton: false
+        })
+      } else if  (!this.state.showSignIn && this.state.email !== ''  && this.state.password !== '' && this.state.name !== '') {
+        this.setState({
+          disableButton: false
+        })
+    }
+  })
+}
 
   handleSubmit = (e) => {
     e.preventDefault()
@@ -47,20 +59,29 @@ export class SignUp extends Component {
     const {name, email, password, showSignIn} = this.state
     const signUpBtnName = showSignIn ? 'Sign In' : 'Sign Up'
     const newUserBtnName = showSignIn ? 'New user? Sign up!' : 'Log in'
+    const popUpMessage = showSignIn ? 'Please Log In' : 'Create Account'
     const errorMessage = showSignIn ? 'email and password do not match' : 'email already exists'
 
     if (this.props.user) return <Redirect to='/' />
     return (
-      <form onSubmit={ this.handleSubmit }>
-      {
-        !showSignIn ? <input name="name" value={name} placeholder='Enter name ...' onChange={ this.handleChange } /> : ''
-      }
-        <input name="email" value={email} placeholder='Enter email ...' onChange={ this.handleChange } className='email-input'/>
-        <input name="password" value={password} placeholder='Enter password ...' onChange={ this.handleChange } />
-        <button className='submit-sign-up' onClick={this.handleSubmit}>{signUpBtnName}</button>        
-        <button className='signin-toggle' onClick={this.handleSignUp}>{newUserBtnName}</button>
-        { this.props.hasErrored ? <h3>{errorMessage}</h3> : undefined }
-      </form> 
+      <div className="form-container">
+        <div className="inner-container">
+          <article className="message-container">
+            <h1>Welcome To Action Flix!</h1>
+            <h3>{popUpMessage}</h3>
+          </article>
+          <form onSubmit={ this.handleSubmit }>
+        {
+          !showSignIn ? <input name="name" value={name} placeholder='Enter name ...' onChange={ this.handleChange } /> : ''
+        }
+          <input name="email" value={email} placeholder='Enter email ...' onChange={ this.handleChange } className='email-input'/>
+          <input name="password" value={password} placeholder='Enter password ...' onChange={ this.handleChange } />
+          <button className='submit-sign-up' disabled={this.state.disableButton} onClick={this.handleSubmit}>{signUpBtnName}</button>        
+          <button className='signin-toggle' onClick={this.handleSignUp}>{newUserBtnName}</button>
+          { this.props.hasErrored ? <h3>{errorMessage}</h3> : undefined }
+          </form> 
+        </div>
+      </div>
     )
   }
 }
