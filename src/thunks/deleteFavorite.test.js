43614fc57259b32615f51e8c6ed9408f0023302b
Joel Smith <joelwsmith123@gmail.com>
2018-12-31 02:59:39 -0700

    Add tests for deleteFavorite thunk

---
+++ src/thunks/deleteFavorite.test.js
@@ -0,0 +1,45 @@
+import { deleteFavorite } from './deleteFavorite'
+import { isLoading, hasErrored } from './../actions'
+
+describe('deleteFavorite', () => {
+  let mockDispatch
+  let mockUserId
+  let mockMovieId
+
+  beforeEach(() => {
+    mockDispatch = jest.fn()
+    mockUserId = 1
+    mockMovieId = 297802
+  })
+
+  it('should call dispatch with isLoading(true)', async () => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      ok: true,
+      json: () => Promise.resolve({
+        user_id: mockUserId,
+        movie_id: mockMovieId
+      })
+    }))    
+
+    const thunk = deleteFavorite(mockUserId, mockMovieId)
+
+    await thunk(mockDispatch)
+
+    expect(mockDispatch).toHaveBeenCalledWith(isLoading(true))
+  })  
+
+  it('should dispatch hasErrored with a message if the response is not ok', async () => {
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      ok: false,
+      statusText: 'there\'s been a fetch error'
+    }))
+
+    const thunk = deleteFavorite(mockUserId, mockMovieId)
+
+    await thunk(mockDispatch)
+
+    expect(mockDispatch).toHaveBeenCalledWith(hasErrored(true))
+  })  
+
+  
+})
