9eb1a823e5c7d953150c2e0ec77daef14be34160
Derek Hanciles <drhanciles@icloud.com>
2019-01-02 10:16:46 -0700

    Updated Header component from functional to class based component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,15 +5,22 @@ import { NavLink } from 'react-router-dom'
 import './Header.css'
 import PropTypes from 'prop-types'
 
-export const Header = (props) =>  {
+export class Header extends Component {
+  constructor() {
+    super()
+    this.state = {
+      welcomeSelected: false
+    }
+  }
+
+  render() {
     return (
       <header className='header'>
         <h1 className='header-title'>Action Flix</h1>
         <NavLink to='/login'><button className='login-btn'>Login</button></NavLink>
-        <button className='logout-btn' onClick={() => props.logUserOut()}>Sign Out</button>
-        <NavLink to='/favorites'><button className='favorites-btn'>Favorites</button></NavLink>
       </header>
     )
+  }
 }
 
 Header.proptypes = {
@@ -25,3 +32,20 @@ export const mapDispatchToProps = (dispatch) => ({
 }) 
 
 export default connect(null, mapDispatchToProps)(Header)
+
+// const loggedIn = (
+//   <header className="header">
+//     <h1 className='header-title'>Action Flix</h1>
+//       <div className='dropdown-wrapper'>
+//         <div className='dropdown-header'>
+//           <div className='dropdown-title'>
+//             <h3 className='welcome-user'>Hello {username.toUpperCase()}</h3>
+//           </div>
+//         </div>
+//         <article className='dropdown-list'>
+//           <button className='logout-btn' onClick={() => props.logUserOut()}>Sign Out</button>
+//           <NavLink to='/favorites'><button className='favorites-btn'>Favorites</button></NavLink>
+//         </article>
+//       </div>
+//   </header>
+// )
