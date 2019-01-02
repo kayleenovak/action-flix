98a1708f566f9306b22eec497ed0b98b6f3fe29c
Derek Hanciles <drhanciles@icloud.com>
2019-01-02 11:12:48 -0700

    Toggle list is now working properly and rederering as expected

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,43 +9,67 @@ export class Header extends Component {
   constructor() {
     super()
     this.state = {
-      welcomeSelected: false
+      listOpen: false
     }
   }
 
+  toggleList = () => {
+    this.setState({
+      listOpen: !this.state.listOpen
+    })
+  }
+
   render() {
-    return (
+    const notLoggedIn = (
       <header className='header'>
         <h1 className='header-title'>Action Flix</h1>
         <NavLink to='/login'><button className='login-btn'>Login</button></NavLink>
-      </header>
+      </header>   
+    )
+  
+  const loggedIn = (
+  <header className="header">
+    <h1 className='header-title'>Action Flix</h1>
+      <div className='dropdown-wrapper'>
+        <div className='dropdown-header'>
+          <div className='dropdown-title'>
+            <h3 className='welcome-user' onClick={() => this.toggleList()}>Hello Bill</h3>
+          </div>
+        </div>
+        { !this.state.listOpen ?  null : 
+          <article className='dropdown-list'>
+            <NavLink to='/favorites'><button className='favorites-btn'>Favorites</button></NavLink>
+            <button className='logout-btn' onClick={() => this.props.logUserOut()}>Sign Out</button>
+          </article>
+          
+        }
+      </div>
+  </header>
+  )
+    if (this.props.user) {
+       return loggedIn
+    }
+    return (
+        notLoggedIn 
     )
   }
+
+
+
 }
+
 
 Header.proptypes = {
   logOut: PropTypes.func.isRequired
 }
 
+export const mapStateToProps = (state) => ({
+  user: state.userId
+})
+
 export const mapDispatchToProps = (dispatch) => ({
   logUserOut: () => dispatch(logOut())
 }) 
 
-export default connect(null, mapDispatchToProps)(Header)
+export default connect(mapStateToProps, mapDispatchToProps)(Header)
 
-// const loggedIn = (
-//   <header className="header">
-//     <h1 className='header-title'>Action Flix</h1>
-//       <div className='dropdown-wrapper'>
-//         <div className='dropdown-header'>
-//           <div className='dropdown-title'>
-//             <h3 className='welcome-user'>Hello {username.toUpperCase()}</h3>
-//           </div>
-//         </div>
-//         <article className='dropdown-list'>
-//           <button className='logout-btn' onClick={() => props.logUserOut()}>Sign Out</button>
-//           <NavLink to='/favorites'><button className='favorites-btn'>Favorites</button></NavLink>
-//         </article>
-//       </div>
-//   </header>
-// )
