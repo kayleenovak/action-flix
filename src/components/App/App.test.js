cadb3d8beb70a8d19ff2c49e9105f8d45162a0cc
Kaylee Bunce <kaybunce@hotmail.com>
2019-01-02 21:57:36 -0700

    Add test for router

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { shallow } from 'enzyme'
+import { Provider } from 'react-redux'
+import { MemoryRouter, Switch, Route } from 'react-router-dom'
+import { shallow, mount } from 'enzyme'
+import configureStore from 'redux-mock-store';
+import { MovieContainer } from '../MovieContainer/MovieContainer'
+import { SignUp } from '../SignUp/SignUp'
 import { App } from './App';
 
 describe('App', () => {
@@ -9,6 +14,25 @@ describe('App', () => {
 
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should route to main page', () => {
+    const initialState = {};
+    const mockStore = configureStore();
+    let store = mockStore(initialState)
+    const mockMovies = [{title: 'Aquaman'}]
+
+    const wrapper = mount(
+      <Provider store={store}>
+        <MemoryRouter initialEntries={['/']}>
+          <Switch>
+            <Route path='/' render={() => <MovieContainer fetchMovies={ jest.fn() } getFavorites={ jest.fn() } userId={1} isLoading={ false } hasErrored={ false } movies={ mockMovies } location='/' />} />
+          </Switch>
+        </MemoryRouter>
+      </Provider>,
+    );
+
+    expect(wrapper.find(MovieContainer).length).toBe(1)
+  })
 })
 
 
