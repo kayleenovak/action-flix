dd5b3e55e7619a1943e8dae2f93f51d22d0f2bb3
Joel Smith <joelwsmith123@gmail.com>
2019-01-02 22:07:00 -0700

    Finish movieReducer test

--- src/reducers/moviesReducer.test.js
+++ src/reducers/moviesReducer.test.js
@@ -10,6 +10,7 @@ describe('moviesReducer', () => {
 
     expect(result).toEqual(expected)
   })
+
   it('should update state with movies requested if our fetch request is successful', () => {
     const mockMovies = movies.results
     const expected = mockMovies
@@ -17,5 +18,23 @@ describe('moviesReducer', () => {
     const result = moviesReducer([], actions.fetchDataSuccess(mockMovies))
 
     expect(result).toEqual(expected)
+  })
+
+  it('should toggle whether a movie is favorited or not', () => {
+    const mockMovies = movies.results
+    const mockFavoriteMovie = {
+                                movieId: 297802,
+                                posterPath: "http://image.tmdb.org/t/p/w185//i2dF9UxOeb77CAJrOflj0RpqJRF.jpg",
+                                title: "Aquaman",
+                                voteAverage: 6.9,
+                                releaseDate: "2018-12-07",
+                                overview: "Arthur Curry learns that he is the heir to the underwater kingdom of Atlantis, and must step forward to lead his people and be a hero to the world.",
+                                favorite: false
+                              }
+    const expected = mockMovies
+
+    const result = moviesReducer(mockMovies, actions.toggleFavorite(mockFavoriteMovie.title))
+
+    expect(result[0].favorite).toEqual(true)
   })
 })
